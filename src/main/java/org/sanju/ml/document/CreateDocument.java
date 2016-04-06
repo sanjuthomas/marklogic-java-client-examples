75956a064aeef19062e017d66937a828721cbec1
sthomas <sanju@sanju.org>
2016-04-06 09:20:18 -0400

    remove the hook

---
+++ src/main/java/org/sanju/ml/document/CreateDocument.java
@@ -0,0 +1,19 @@
+package org.sanju.ml.document;
+
+import org.sanju.ml.document.pojo.DummyDocument;
+
+/**
+ *
+ * @author Sanju Thomas
+ *
+ */
+public class CreateDocument {
+
+	public void create(final String uri, final DummyDocument dummy){
+
+
+
+	}
+
+
+}
