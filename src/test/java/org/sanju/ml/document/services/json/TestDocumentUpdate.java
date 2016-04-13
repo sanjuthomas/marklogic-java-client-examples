3cef8ad5839bb8df67aebc79e48d23441e80c3f8
sthomas <sanju@sanju.org>
2016-04-13 15:35:40 -0400

    adding extra test cases

--- src/test/java/org/sanju/ml/document/services/json/TestDocumentUpdate.java
+++ src/test/java/org/sanju/ml/document/services/json/TestDocumentUpdate.java
@@ -15,17 +15,29 @@ public class TestDocumentUpdate {
 
 	private JSONDocumentService documentService;
 
+	DummyDocument dummyDocument;
+
 	@Before
 	public void setUp(){
 		this.documentService = new JSONDocumentService();
+		this.dummyDocument = new DummyDocument(1000, "Test Document from Save Test");
+		this.documentService.save(this.dummyDocument);
 	}
 
+
 	@Test
-	public void shouldSave(){
-		final DummyDocument dummyDocument = new DummyDocument(1000, "Test Document from Save Test");
-		this.documentService.save(dummyDocument);
-		final DummyDocument dummyDocumentFromDB = this.documentService.find(new DummyDocument(1000));
+	public void shouldUpdate(){
+
+		DummyDocument dummyDocumentFromDB = this.documentService.find(new DummyDocument(1000));
 		assertEquals(1000, dummyDocumentFromDB.getId());
 		assertEquals("Test Document from Save Test", dummyDocumentFromDB.getName());
+
+		this.dummyDocument.setName("This is a updated document");
+		this.documentService.save(this.dummyDocument);
+
+		dummyDocumentFromDB = this.documentService.find(new DummyDocument(1000));
+		assertEquals(1000, dummyDocumentFromDB.getId());
+		assertEquals("This is a updated document", dummyDocumentFromDB.getName());
+
 	}
 }
